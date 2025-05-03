726bad88ca404aa4e6c1e2358b33419fafc5a857
kaortojites <kaortojites@gmail.com>
2025-05-04 04:08:36 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     },
     verification: {
         google: 'p5tuF_hUhyODSsm7s-66naHQvq1Bz2TttWoZvbw0PzQ',
-        yandex: 'afeedbd84f54b37e',
+        yandex: 'dd8c41b747118bec',
     },
 };
 
@@ -46,17 +46,6 @@ export default function RootLayout({
             },
             "foundingDate":"2024-09-16"
         }
-        const jsonLd1 = {
-            "@context": "https://schema.org",
-        "@type": "WebPage",
-        "name": `${SITENAME}`,
-        "url": "https://mangolivebugil.pages.dev",
-        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
-        "image": "https://mangolivebugil.pages.dev/favicon.ico",
-        "potentialAction":{
-            "@type":"ReadAction",
-            "target":"https://mangolivebugil.pages.dev"}
-        }
         const jsonLd2 = {
             "@context": "https://schema.org",
         "@type": "WebSite",
@@ -80,10 +69,6 @@ export default function RootLayout({
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
         />
         <script
           type="application/ld+json"
